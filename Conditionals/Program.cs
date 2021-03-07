fd8f42ba50a5fdf0bcdb9362a5a137cd289b0303
elif-cloud <74643711+elif-cloud@users.noreply.github.com>
2021-03-07 10:15:34 +0300

    Add project files.

---
+++ Conditionals/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 20;
+
+            //if (number==10)
+            //{
+            //    Console.WriteLine("Number is 10");
+            //}
+            //else if(number==20)
+            //{
+            //    Console.WriteLine("Number is 20");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Number isn't 10");
+            //}
+            //Console.WriteLine(number == 10 ? "Number is 10" : "Number is not 10");
+
+
+            //switch (number)
+            //{
+            //    case 10:
+            //        Console.WriteLine("number is 10");
+            //        break;
+            //    case 20:
+            //        Console.WriteLine("number is 10");
+            //        break;
+            //    default:
+            //        Console.WriteLine("not 10 and 20");
+            //        break;
+            //        //ctrl k + d satırları düzenler
+            //}
+
+            if (number >= 0 && number <= 100)
+            {
+                Console.WriteLine("number is between 0-100");
+            }
+            else if (number > 100 && number <= 200)
+            {
+                Console.WriteLine("number is betwee 101-200");
+            }
+            else if (number > 200 || number < 0)
+            {
+                Console.WriteLine("Number is less than 0 or greater than 200");
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ cSharpWithBTK/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpWithBTK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            char character = 'A';
+            string city = "Ben bir metinim :) "; //karakterleren oluşan bir dizidir yani referans tiptir!!
+            bool condition = false;
+            double number5 = 5.655;       //64 bit
+            decimal number6 = 9.444545m; //tam sayı aldığında m/M harfi konulmaz. Double a göre daha hassas.
+            byte number4 = 255;          //8 bit 0-255 arasında
+            short number3 = -5455;       //16 bit
+            int number1 = 10;            //32 bit
+            long number2 = 1000545;      //64 bit
+            var number7 = 10;
+            number7 = 'B'; //number7 artık hep int tutar!!!
+            Console.WriteLine("Number: {0}", number1);
+            Console.WriteLine("Number: {0}", number2);
+            Console.WriteLine("Number: {0}", number3);
+            Console.WriteLine("Number: {0}", number4);
+            Console.WriteLine("Number: {0}", number5);
+            Console.WriteLine("Number: {0}", number6);
+            Console.WriteLine("Number: {0}", number7);
+            Console.WriteLine("Number: {0}", condition);
+            Console.WriteLine("Number: {0}", city);
+            Console.WriteLine("Character: {0}", (int)character);
+            Console.WriteLine((int)Days.Friday);
+            Console.ReadLine();
+        }
+    }
+    enum Days
+    {
+        Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+        //Alt alta stringsel ifadeler için kolaylık sağlar
+
+    }
+}
