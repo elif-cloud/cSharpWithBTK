2db6b38e90f42edc284da65772cd6753d03da6da
elifcanci <74643711+elif-cloud@users.noreply.github.com>
2021-03-21 20:26:22 +0300

    21.03.2021

---
+++ Inheritance/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3]
+            {
+                new Customer
+                {
+                    FirtName="Elif"
+                }, new Student
+                {
+                    FirtName="Safiye"
+                }, new Person
+                {
+                    FirtName="Şahin"
+                }
+            };
+            foreach (var Person in persons)
+            {
+                Console.WriteLine(Person.FirtName);
+            }
+
+            Console.ReadLine();
+        }
+
+        class Person
+        {
+            public int Id { get; set; }
+            public string FirtName { get; set; }
+            public string LastName { get; set; }
+        }
+         
+        class Customer:Person
+        {
+            public string City { get; set; }      
+        }
+
+        class Student:Person
+        {
+            public String Department { get; set; }
+        }
+    }
+}
---
+++ VirtualMethods/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlServer sqlServer = new SqlServer();
+            sqlServer.Add();
+            MySqlServer mysql = new MySqlServer();
+            mysql.Add();
+            Console.ReadLine();
+        }
+
+        class Database
+        {
+            public virtual void Add()
+            {
+                Console.WriteLine("Added by default");
+            }
+
+            public virtual void Delete()
+            {
+                Console.WriteLine("Deleted by default");
+            }
+        }
+
+        class SqlServer:Database
+        {
+            public override void Add() //virtual: üstteki (Add) methodu geçersiz kılar
+            {
+                Console.WriteLine("Added by Sql Code"); //override: geçersiz kılmak
+                //base.Add();
+            }
+        }
+
+        class MySqlServer:Database
+        {
+            public override void Add()
+            {
+                Console.WriteLine("Added by Mysql Code");
+            }
+        }
+    }
+}
